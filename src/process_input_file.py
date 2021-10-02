90a1593108e672f3239a388d2969fc0404a6b02d
John Grando <jgrando@gard.com>
2021-10-01 22:41:19 -0400

    setup logger command line options

--- src/process_input_file.py
+++ src/process_input_file.py
@@ -1,4 +1,51 @@
+import argparse
+import logging
+import re
+import os
 from logger import Logger
+
+
+def get_property(prop):
+    """
+    Get property value from __init__.py file in src directory
+    :param prop: Property name
+    :return: Return value for a given property
+    """
+    try:
+        result = re.search(
+            r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+            open(os.path.join(os.path.dirname(__file__), '__init__.py')).read())
+        output = result.group(1)
+    except AttributeError:
+        output = '{} could not be found'.format(prop)
+    return output
+
+
+def build_parser():  # pragma: no cover
+    """
+    Build argument parser.
+    """
+    parser = argparse.ArgumentParser(
+        prog='ASHRAE 140 Automation',
+        description='Automated report generation of ASHRAE 140 Verification')
+    parser.add_argument(
+        '--logger_level',
+        '-l',
+        nargs='?',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        default='WARNING',
+        help='Specify logger level')
+    parser.add_argument(
+        '--version',
+        '-v',
+        action='store_true',
+        help='Display version information')
+    parser.add_argument(
+        '--write_logs',
+        '-wl',
+        action='store_true',
+        help='Write logs to file')
+    return parser
 
 
 class InputProcessor(Logger):
@@ -11,6 +58,25 @@ class InputProcessor(Logger):
         return
 
 
+def main(args=None):
+    if hasattr(args, 'version') and args.version:
+        version = get_property('__version__')
+        print('ASHRAE 140 Automation Version: {}'.format(version))
+        return
+    # set the arg defaults for testing when Namespace is used
+    if not hasattr(args, 'logger_level'):
+        args.logger_level = 'WARNING'
+    if getattr(args, 'write_logs', None):
+        logger_name = 'file_logger'
+    else:
+        logger_name = 'console_only_logger'
+    ip = InputProcessor(logger_level=args.logger_level, logger_name=logger_name)
+    ip.logger.info('test')
+    return
+
+
 if __name__ == "__main__":
-    ip = InputProcessor(logger_level='DEBUG')
-    ip.logger.info('Test')
+    ip_parser = build_parser()
+    ip_parser_args, unknown_args = ip_parser.parse_known_args()
+    main(ip_parser_args)
+    logging.shutdown()
