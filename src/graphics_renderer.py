e6e8926301b4cfafa34425f6d6e5d077b51f1a2f
jcyuan <jinchaoyuan@gmail.com>
2023-05-05 09:05:48 -0500

    Trial 1 to get additional columns.

--- src/graphics_renderer.py
+++ src/graphics_renderer.py
@@ -2024,9 +2024,9 @@ class GraphicsRenderer(Logger):
         # get data
         data = {}
         for idx, (tst, json_obj) in enumerate(self.json_data.items()):
-            data['Average'] = {'C': json_obj['sky_temperature_output']['600']['Average']['C']}
-            data['Minimum'] = {'C': json_obj['sky_temperature_output']['600']['Minimum']['C']}
-            data['Maximum'] = {'C': json_obj['sky_temperature_output']['600']['Maximum']['C']}
+            data['Average'] = {'C': [json_obj['sky_temperature_output']['600']['Average']['C'], '', '']}
+            data['Minimum'] = {'C': [json_obj['sky_temperature_output']['600']['Minimum']['C'], json_obj['sky_temperature_output']['600']['Minimum']['Hour'], json_obj['sky_temperature_output']['600']['Minimum']['Month']]}
+            data['Maximum'] = {'C': [json_obj['sky_temperature_output']['600']['Maximum']['C'], json_obj['sky_temperature_output']['600']['Maximum']['Hour'], json_obj['sky_temperature_output']['600']['Maximum']['Month']]}
             df_obj = pd.DataFrame.from_dict(data)
             df_obj['software'] = json_obj['identifying_information']['software_name']
             software_array.append(json_obj['identifying_information']['software_name'])
